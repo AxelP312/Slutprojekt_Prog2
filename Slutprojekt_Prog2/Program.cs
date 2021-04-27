7649feb0e3646a2df2b5d7a6b59ed3c32abb534e
AxelP312 <axepetaxe@gmail.com>
2021-04-27 10:01:34 +0200

    MasterCommit

---
+++ Slutprojekt_Prog2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Slutprojekt_Prog2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
